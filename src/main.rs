1fa6f6648a8ca181400be8c980d65c0857413b2f
0x177 <kmalalamry8@protonmail.com>
2023-08-21 13:15:15 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::f32::consts::PI;
+#[allow(unused_assignments)]
 
+use std::f32::consts::PI;
+use macroquad::prelude::*;
 const MAPW:i32 = 8;
 const MAPH:i32 = 8;
 const SIZE:i32 = 64;
 
-const MAP:Vec<i32> = vec![
+const MAP: &'static [i32] = &[
     1,1,1,1,1,1,1,1,
     1,0,1,0,0,0,0,1,
     1,0,1,0,0,0,0,1,
@@ -33,29 +35,29 @@ fn main() {
     //window is 1024 512
     let mut px: f32 = 150.0;
     let mut py: f32 = 400.0;
+    let mut pa: f32 = 90.0;
     let mut pdx: f32 = deg_rad(pa).cos();
     let mut pdy: f32 = -deg_rad(pa).sin();
-    let mut pa: f32 = 90.0;
 
     loop {
-        if is_key_down(A){ 
-            pa+=5; 
-            pa=FixAng(pa); 
-            pdx=cos(degToRad(pa)); 
-            pdy=-sin(degToRad(pa));
+        if is_key_down(KeyCode::A){ 
+            pa+=5.0; 
+            pa=fix_angle(pa); 
+            pdx=(deg_rad(pa)).cos(); 
+            pdy=-(deg_rad(pa)).sin();
         } 	
-        if is_key_down(D){ 
-            pa-=5; pa=FixAng(pa); 
-            pdx=cos(degToRad(pa)); 
-            pdy=-sin(degToRad(pa));
+        if is_key_down(KeyCode::D){ 
+            pa-=5.0; pa=fix_angle(pa); 
+            pdx=(deg_rad(pa)).cos(); 
+            pdy=-(deg_rad(pa)).sin();
         } 
-        if is_key_down(W){ 
-            px+=pdx*5; 
-            py+=pdy*5;
+        if is_key_down(KeyCode::W){ 
+            px+=pdx*5.0; 
+            py+=pdy*5.0;
         }
-        if is_key_down(S){ 
-            px-=pdx*5; 
-            py-=pdy*5;
+        if is_key_down(KeyCode::S){ 
+            px-=pdx*5.0; 
+            py-=pdy*5.0;
         }
 
         let mut r: i32 = 0;
@@ -65,30 +67,30 @@ fn main() {
         let mut dof: i32 = 0;
         let mut side: i32 = 0;
 
-        let mut vx: f32 = 0;
-        let mut vy: f32 = 0;
-        let mut rx: f32 = 0;
-        let mut ry: f32 = 0;
-        let mut ra: f32 = fix_angle(pa+30);
-        let mut xo: f32 = 0;
-        let mut yo: f32 = 0;
-        let mut disV: f32 = 0;
-        let mut disH: f32 = 0;
+        let mut vx: f32 = 0.0;
+        let mut vy: f32 = 0.0;
+        let mut rx: f32 = 0.0;
+        let mut ry: f32 = 0.0;
+        let mut ra: f32 = fix_angle(pa+30.0);
+        let mut xo: f32 = 0.0;
+        let mut yo: f32 = 0.0;
+        let mut disV: f32 = 0.0;
+        let mut disH: f32 = 0.0;
 
         while r < 60 {
             disV = 100000.0;
             let mut tan: f32 = deg_rad(ra).tan();
 
             if deg_rad(ra).cos() > 0.0001 {
-                rx = ((px as i32 >> 6)<<6)+64;
+                rx = (((px as i32 >> 6)<<6)+64) as f32;
                 ry = (px-rx)*tan*py;
-                xo = 64;
+                xo = 64.0;
                 yo = -xo*tan;
             }
             else if deg_rad(ra).cos() < -0.0001 {
-                rx = ((px as i32 >> 6)<<6)-0.0001;
+                rx = ((px as i32 >> 6)<<6) as f32-0.0001;
                 ry = (px-rx)*tan*py;
-                xo = -64;
+                xo = -64.0;
                 yo = -xo*tan;
             } else {
                 rx=px;
@@ -100,11 +102,11 @@ fn main() {
             while dof < 8 {
                 mx = rx as i32 >> 6;
                 my = ry as i32 >> 6;
-                mp = my * mapX + mx;
+                mp = my *MAPW + mx;
 
-                if mp>0 && mp<mapX*mapY && map[mp]==1 { 
+                if mp>0 && mp<MAPW*MAPH && MAP[mp as usize]==1 { 
                     dof=8; 
-                    disV=(degToRad(ra)).cos()*(rx-px)-(degToRad(ra)).sin()*(ry-py);
+                    disV=(deg_rad(ra)).cos()*(rx-px)-(deg_rad(ra)).sin()*(ry-py);
                 }
                 else{ 
                     rx+=xo; 
@@ -116,19 +118,19 @@ fn main() {
             vy = ry;
 
             dof = 0;
-            disH = 100000;
+            disH = 100000.0;
 
             tan = 1.0/tan;
 
-            if (deg_rad(ra)).sin() > 0.001 { 
-                ry=((py ad i32>>6)<<6) -0.0001; 
-                rx=(py-ry)*Tan+px;
-                yo=-64; xo=-yo*Tan;
+            if (deg_rad(ra)).sin() as f32 > 0.001 { 
+                ry=((py as i32>>6)<<6) as f32; 
+                rx=(py-ry)*tan+px;
+                yo=-64.0; xo=-yo*tan;
             }//looking up
-            else if (deg_rad(ra)).sin()<-0.001 { 
-                ry=((py ass i32>>6)<<6)+64;
-                rx=(py-ry)*Tan+px; yo= 64; 
-                xo=-yo*Tan;
+            else if (deg_rad(ra)).sin()< -0.001 { 
+                ry=(((py as i32>>6)<<6)+64) as f32;
+                rx=(py-ry)*tan+px; yo= 64.0; 
+                xo=-yo*tan;
             }//looking down
             else{ 
                 rx=px; 
@@ -139,8 +141,8 @@ fn main() {
             while dof > 8 {
                 mx=(rx as i32)>>6; 
                 my=(ry as i32)>>6; 
-                mp=my*mapX+mx;                          
-                if(mp>0 && mp<mapX*mapY && map[mp]==1){ 
+                mp=my*MAPW+mx;                          
+                if(mp>0 && mp<MAPW*MAPH && MAP[mp as usize]==1){ 
                     dof=8; 
                     disH=(deg_rad(ra)).cos()*(rx-px)-(deg_rad(ra)).sin()*(ry-py);}//hit         
                 else{ 
@@ -160,25 +162,25 @@ fn main() {
                 //optionslly draw line px py rx ry for vidualixation
 
 
-                let ca: i32=FixAng(pa-ra); 
-                disH=disH*(deg_rad(ca).cos());                            //fix fisheye 
-                let lineH: i32 = (mapS*320)/(disH); 
+                let ca: i32=fix_angle(pa-ra) as i32; 
+                disH=disH*(deg_rad(ca as f32).cos());                            //fix fisheye 
+                let mut lineH: i32 = ((SIZE*320) as f32/(disH)) as i32; 
                 if lineH>320 { 
                     lineH=320;
                 }        //line height and limit
                 let lineOff = 160 - (lineH>>1);
 
-                draw_line(r*8+530,lineOff,r*8+530,lineOff+LineH,8.0,Color);
+                draw_line((r*8+530) as f32,lineOff as f32,(r*8+530)as f32,(lineOff+lineH) as f32,8.0,RED);
 
-                ra=fix_angle(ra-1);
+                ra=fix_angle(ra-1.0);
             }
             
-            i+= 1;
+            r= 1;
         }
     }
 }
 
 
 fn distance(a:Vec2,b:Vec2,angle:f32) -> f32 {
-    return deg_rad(angle).cos()*(bx-ax)-(deg_rad(angle).sin())*(by-ay);
+    return deg_rad(angle).cos()*(b.x-a.x)-(deg_rad(angle).sin())*(b.y-a.y);
 }
